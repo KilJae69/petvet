068ea6ed04660b0e00a16c233427d98761c412b5
KilJae69 <adi.toromanovic@outlook.com>
2025-01-25 11:25:05 +0100

    .

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      { source: "/sitemap.xml", destination: "/sitemap" },
+      { source: "/robots.txt", destination: "/robots" },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);
